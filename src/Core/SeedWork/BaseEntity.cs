09639b38b1259e21f37f661141face300bb3ae7f
Wesley Antonio de Santana <wesley.santana@seventh.com.br>
2023-05-12 19:18:06 -0300

    Inclusão de datas no BaseEntity

--- src/Core/SeedWork/BaseEntity.cs
+++ src/Core/SeedWork/BaseEntity.cs
@@ -3,4 +3,7 @@
 public abstract class BaseEntity : BaseValidate
 {
     public int Id { get; protected set; }
+    public DateTime DataCriacao { get; protected set; }
+    public DateTime? DataAlteracao { get; protected set; }
+    public DateTime? DataExclusao { get; protected set; }
 }
