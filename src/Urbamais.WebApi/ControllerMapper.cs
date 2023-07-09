479a35bb1f40bb4b4d70885f03c9df6c1b347047
Wesley Antonio de Santana <wesley.santana@seventh.com.br>
2023-07-09 17:41:19 -0300

    ControllerMapper

---
+++ src/Urbamais.WebApi/ControllerMapper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace Urbamais.WebApi;
+
+public static class ControllerMapper
+{
+    public static void MapControllers(ApplicationPartManager applicationPartManager)
+    {
+        // Obtém todos os recursos de partes do aplicativo que contêm controllers
+        var feature = new ControllerFeature();
+        applicationPartManager.PopulateFeature(feature);
+
+        // Itera sobre cada controller encontrado
+        foreach (var controllerType in feature.Controllers)
+        {
+            // Use o controllerType para realizar qualquer ação desejada,
+            // como registrar o controller em algum container de injeção de dependência
+            // ou adicionar rotas personalizadas.
+
+            var teste = controllerType.FullName;
+            Console.WriteLine("Controller mapeado: " + controllerType.FullName);
+        }
+    }
+}
---
+++ src/Urbamais.WebApi/Controllers/v1/PerfilController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Urbamais.Application.ViewModels.Response.v1.Unidade;
+
+namespace Urbamais.WebApi.Controllers.v1;
+
+[Route("api/[controller]")]
+[ApiController]
+[ApiVersion("1.0")]
+public class PerfilController : ControllerBase
+{
+    //[HttpGet]
+    //public async Task<List<IActionResult>> Get()
+    //{
+
+    //}
+}
